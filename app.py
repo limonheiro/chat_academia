9fb75dc06f6cf1ed9c2bd53a9c3dee982183f78b
Nicolas <nicolastimoteu@gmail.com>
2024-12-11 22:10:51 +0000

    comentários alterados

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import time
 import boto3
 from botocore.exceptions import ClientError
 
-# Criar o client Bedrock Runtime na região AWS de uso.
+# Criar o cliente Bedrock Runtime na região da AWS de uso.
 client = boto3.client("bedrock-runtime", region_name="us-east-1")
 
 model_id = "amazon.titan-text-premier-v1:0"
 
-    
-# Streamed response emulator
+# Envia a resposta do chatbot
 def response_generator():
     conversation = [
         {
@@ -24,32 +23,32 @@ def response_generator():
         inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
     )
 
-    # Extrair e printar a resposta.
+    # Extrair e imprimir a resposta.
     response = response["output"]["message"]["content"][0]["text"]
     
     for word in response.split():
         yield word + " "
-        time.sleep(0.05)
+        time.sleep(0.06)
 
 st.title("Academia")
 
-# Initialize chat history
+# Inicializar o histórico de chat
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
+# Exibir as mensagens do chat do histórico quando o app for reiniciado
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
+# Aceitar a entrada do usuário
+if prompt := st.chat_input("O que está acontecendo?"):
+    # Exibir a mensagem do usuário no container de mensagens de chat
     with st.chat_message("user"):
         st.markdown(prompt)
     
-    # Display assistant response in chat message container
+    # Exibir a resposta do assistente no container de mensagens de chat
     with st.chat_message("assistant"):
         response = st.write_stream(response_generator())
-    # Add assistant response to chat history
+    # Adicionar a resposta do assistente ao histórico de chat
     st.session_state.messages.append({"role": "assistant", "content": response})
